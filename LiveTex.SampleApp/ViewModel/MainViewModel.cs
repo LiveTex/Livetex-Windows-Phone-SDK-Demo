71ac238966a530145a25deacc6a333023ced4354
Смитюх Вячеслав <smityukh@starline.ru>
2014-12-22 14:14:33 +0300

    На стартовую страницу добавлены поля с параметрами подключения

--- LiveTex.SampleApp/ViewModel/MainViewModel.cs
+++ LiveTex.SampleApp/ViewModel/MainViewModel.cs
@@ -10,10 +10,14 @@ namespace LiveTex.SampleApp.ViewModel
 		: ViewModel
 	{
 		private const string cKeyKey = "ApplicationKey";
+		private const string cAppIDKey = "ApplicationID";
+		private const string cAuthUriKey = "AuthenticatedUriKey";
 
 		protected override Task Initialize()
 		{
 			Key = Storage.GetValue(cKeyKey, Config.cKey);
+			AppID = Storage.GetValue(cAppIDKey, Config.cApplicationID);
+			AuthUri = Storage.GetValue(cAuthUriKey, Config.cAuthServiceUri);
 
 			return Task.FromResult(true);
 		}
@@ -23,6 +27,20 @@ namespace LiveTex.SampleApp.ViewModel
 		{
 			get { return _key; }
 			set { SetValue(ref _key, value); }
+		}
+
+		private string _appID;
+		public string AppID
+		{
+			get { return _appID; }
+			set { SetValue(ref _appID, value); }
+		}
+
+		private string _authUri;
+		public string AuthUri
+		{
+			get { return _authUri; }
+			set { SetValue(ref _authUri, value); }
 		}
 
 		private DelegateCommand _removeTokenCommand;
@@ -59,17 +77,31 @@ namespace LiveTex.SampleApp.ViewModel
 				{
 					if (string.IsNullOrWhiteSpace(Key))
 					{
-						throw new Exception("Код не задан");
+						throw new Exception("Key не задан");
 					}
 
-					if(!Equals(Storage.GetValue<string>(cKeyKey), Key))
+					if (string.IsNullOrWhiteSpace(AppID))
+					{
+						throw new Exception("ApplicationID не задан");
+					}
+
+					if (string.IsNullOrWhiteSpace(AuthUri))
+					{
+						throw new Exception("Authentication Uri не задан");
+					}
+
+					if (!Equals(Storage.GetValue<string>(cKeyKey), Key)
+						|| !Equals(Storage.GetValue<string>(cAppIDKey), AppID)
+						|| !Equals(Storage.GetValue<string>(cAuthUriKey), AuthUri))
 					{
 						LiveTexClient.RemoveToken();
 					}
 
 					Storage.SetValue(cKeyKey, Key);
+					Storage.SetValue(cAppIDKey, AppID);
+					Storage.SetValue(cAuthUriKey, AuthUri);
 
-					await LiveTexClient.Initialize(Key, Config.cApplicationID, Config.cAuthServiceUri);
+					await LiveTexClient.Initialize(Key, AppID, AuthUri);
 
 					var dialogState = await LiveTexClient.Client.GetDialogStateAsync();
 
